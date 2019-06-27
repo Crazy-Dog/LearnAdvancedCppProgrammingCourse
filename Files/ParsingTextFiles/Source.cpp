d76814c79cf548c4d2f4640f2a41bae46840fb0d
Crazy-Dog <ilya-drachev@mail.ru>
2019-06-27 22:00:21 +0300

    parsing text files lesson added

---
+++ Files/ParsingTextFiles/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	ifstream input;
+	string filename = "stats.txt";
+
+	input.open(filename);
+
+	if (!input.is_open())
+	{
+		return 1;
+	}
+
+	while (input)
+	{
+		string line;
+
+		getline(input, line, ':');
+
+		int population;
+		input >> population;
+
+		input >> ws;
+
+		cout << "'" << line << "' -- '" << population << "'" << endl;
+	}
+
+	input.close();
+
+	getchar();
+
+	return 0;
+}
